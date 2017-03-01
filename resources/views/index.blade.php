846048cb88755284b3c73eb1570fbd22ad781084
horseman12 <1256011392@qq.com>
2017-03-01 20:01:46 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -127,7 +127,7 @@
 
 <div class="head2">
     <a href"/">首页</a>&nbsp;&nbsp; <a href="quality">精选集</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="rank_index">排行榜</a>&nbsp;&nbsp;
-    <a href="ac_index">艺人</a>&nbsp;&nbsp;<a href="javascript:void(0)">mv</a>&nbsp;&nbsp;<a href="javascript:void(0)">分类</a>
+    <a href="ac_index">艺人</a>&nbsp;&nbsp;<a href="javascript:void(0)">mv</a>&nbsp;&nbsp;<a href="type">分类</a>
 </div>
 
 <div class="focusBox">
